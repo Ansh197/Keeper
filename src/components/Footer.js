ae1070e5f8c21c16f160d1c67456af4aa384dbc7
Ansh197 <77569137+Ansh197@users.noreply.github.com>
2023-12-15 22:41:02 +0530

    basic structure ready

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const date = new Date().getFullYear();
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <p>Copyright @ {date}</p>
+    </div>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <div className='header'>
+        <h1>Keeper</h1>
+    </div>
+  )
+}
